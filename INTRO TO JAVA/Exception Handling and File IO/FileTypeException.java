274eaa5b54b789f3890b79732d160f9f1c1df85c
Robert12354z <49956764+Robert12354z@users.noreply.github.com>
2022-02-24 21:05:22 -0800

    Create FileTypeException.java

---
+++ INTRO TO JAVA/Exception Handling and File IO/FileTypeException.java
@@ -0,0 +1,16 @@
+package hw08;
+/* Name: Roberto Reyes
+	 * CIN: 307806805
+	 * Course & Section: CS2012 Sections 01 & 02 
+	 * Description: Checks to see if the file type is csv
+	 */
+public class FileTypeException extends Exception {
+	
+	public FileTypeException() {
+		super();
+
+	}
+	public FileTypeException(String errorMsg) {
+		super(errorMsg);
+		}
+	}
